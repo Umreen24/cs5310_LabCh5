5683affa9f617cfb680aa2fe97e2b9b00c452a56
Umreen <uidigitalcrafts@gmail.com>
2021-03-04 16:49:16 -0600

    Creating global functions to run steps for both subjects 1 & 2

--- ui_labch5.py
+++ ui_labch5.py
@@ -5,9 +5,9 @@ Spyder Editor
 This is a temporary script file.
 """
 
-"""
+"""""""""""""""
 IMPORTING PACKAGES
-"""
+"""""""""""""""
 import os
 import math
 import pandas as pd
@@ -23,50 +23,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 
-"""
-SUBJECT 1 TASKS
-"""
-"""
-Q1 - Load the data in each of the Matlab files into a data frame.
-"""
-# Setting current directory and getting files
-os.chdir('/Users/umreenimam/Documents/Masters/Masters_Classes/CS_5310/chapter05/lab_data/Data/Subject1')
-pre_file = 'Pre.mat'
-med_file = 'Med.mat'
-post_file = 'Post.mat'
+"""""""""""""""
+FUNCTIONS
+"""""""""""""""
+# Remove zero-fil-in rows
+def remove_zeros(data):
+    new_data = data[~np.all(data == 0, axis = 1)]
+    return new_data
 
-# Loading data 
-pre_data = loadmat(pre_file)
-med_data = loadmat(med_file)
-post_data = loadmat(post_file)
-
-# Extracting data
-pre_ = pre_data['data']
-med_ = med_data['data']
-post_ = post_data['data']
-
-# Create dataframes and transpose them 
-pre_t = pd.DataFrame(pre_.T)
-med_t = pd.DataFrame(med_.T)
-post_t = pd.DataFrame(post_.T)
-
-"""
-Q2 - Remove zero-fill-in rows from each data frame.
-"""
-#pre_t = pre_t.loc[(pre_t != 0).any(axis = 1)]
-#med_t = med_t.loc[(med_t != 0).any(axis = 1)]
-#post_t = post_t.loc[(post_t != 0).any(axis = 1)]
-
-pre_t = pre_t[~np.all(pre_t == 0, axis = 1)]
-med_t = med_t[~np.all(med_t == 0, axis = 1)]
-post_t = post_t[~np.all(post_t == 0, axis = 1)]
-
-"""
-Q3 - Compute alpha PSIs and spectral entropies.
-"""
-# Creating function to calculate psis and spectral entropies
+# Calculate psis & spectral entropies
 def get_psi_entropies(data):
-    
     # Setting variables needed for for loop
     band = [0.5, 4, 7, 12, 30, 100]
     fs = 1024
@@ -98,22 +64,318 @@ def get_psi_entropies(data):
     
     return alpha_psi_df
 
+# Create state labels
+def create_labels(pre_data, med_data, post_data): 
+    pre_label = ["Pre"] * pre_data.shape[0]
+    med_label = ["Med"] * med_data.shape[0]
+    post_label = ["Post"] * post_data.shape[0]
+    
+    state_labels = pre_label + med_label + post_label
+    
+    return state_labels
+
+# Combining datasets
+def combine_datasets(data1, data2, data3):
+    frames = [data1, data2, data3]
+    new_df = pd.concat(frames, ignore_index = True)
+    
+    return new_df
+
+# Remove co-linearity
+def remove_colinearity(data, neg_threshold):
+    corr_mat = data.corr()
+    row = corr_mat.shape[0]
+    column = corr_mat.shape[1]
+    
+    correlated_features = []
+    
+    for x in range(row): 
+        for y in range(column):
+            if x == y:
+                break
+            if corr_mat.iloc[x, y] > abs(neg_threshold) or corr_mat.iloc[x, y] < neg_threshold:
+                correlated_features.append(corr_mat.columns[x])
+                break
+
+    return corr_mat, correlated_features
+
+# Normalize data using min max scaler
+def min_max(data): 
+    scaler = MinMaxScaler()
+    normalized_corr = scaler.fit_transform(data)
+    normalied_corr_df = pd.DataFrame(normalized_corr)
+    
+    return normalied_corr_df
+
+"""""""""""""""
+SUBJECT 1 TASKS
+"""""""""""""""
+"""
+Q1 - Load the data in each of the Matlab files into a data frame.
+"""
+# Setting current directory and getting files
+os.chdir('/Users/umreenimam/Documents/Masters/Masters_Classes/CS_5310/chapter05/lab_data/Data/Subject1')
+sub1_pre_file = 'Pre.mat'
+sub1_med_file = 'Med.mat'
+sub1_post_file = 'Post.mat'
+
+# Loading data 
+sub1_pre_data = loadmat(sub1_pre_file)
+sub1_med_data = loadmat(sub1_med_file)
+sub1_post_data = loadmat(sub1_post_file)
+
+# Extracting data
+sub1_pre_ = sub1_pre_data['data']
+sub1_med_ = sub1_med_data['data']
+sub1_post_ = sub1_post_data['data']
+
+# Create dataframes and transpose them 
+sub1_pre_t = pd.DataFrame(sub1_pre_.T)
+sub1_med_t = pd.DataFrame(sub1_med_.T)
+sub1_post_t = pd.DataFrame(sub1_post_.T)
+
+"""
+Q2 - Remove zero-fill-in rows from each data frame.
+"""
+# Calling remove_zeros function
+sub1_pre = remove_zeros(sub1_pre_t)
+sub1_med = remove_zeros(sub1_med_t)
+sub1_post = remove_zeros(sub1_post_t)
+
+"""
+Q3 - Compute alpha PSIs and spectral entropies.
+"""
 # Calling alpha psi function on each dataframe
-pre_df = get_psi_entropies(pre_t)
-med_df = get_psi_entropies(med_t)
-post_df = get_psi_entropies(post_t)
+sub1_pre_df = get_psi_entropies(sub1_pre)
+sub1_med_df = get_psi_entropies(sub1_med)
+sub1_post_df = get_psi_entropies(sub1_post)
 
 """
 Q4 & Q5 - Create list of brain state labels & combine dataframes into one.
 """
-pre_label = ["Pre"] * pre_df.shape[0]
-med_label = ["Med"] * med_df.shape[0]
-post_label = ["Post"] * post_df.shape[0]
+# Calling create_labels function
+sub1_state_labels = create_labels(sub1_pre_df, sub1_med_df, sub1_post_df)
 
-frames = [pre_df, med_df, post_df]
-sub1_df = pd.concat(frames, ignore_index = True)
-state_labels = pre_label + med_label + post_label
+# Calling combine_datasets function
+sub1_df = combine_datasets(sub1_pre_df, sub1_med_df, sub1_post_df)
 
 """
-Q6-Q8 - Create a correlation coefficient matrix of 68 channels.
+Q6-Q8 - Create a correlation coefficient matrix of 68 channels, remove co-linearity, 
+and print new correlation matrix.
 """
+# Running co-linearity function
+corrleation_mat, cols_to_remove = remove_colinearity(sub1_df, -0.9)
+sub1_df_remain = sub1_df.drop(columns = cols_to_remove, axis = 1)
+
+# Correlation matrix of remaining data 
+sub1_corr = sub1_df_remain.corr()
+
+# Heatmaps of data before removing co-linearity & after removing 
+# co-linearity
+sns.set_theme(style = "white")
+
+plt.figure(figsize=(8,8))
+color_map = sns.diverging_palette(230, 20, as_cmap = True)
+sns.heatmap(corrleation_mat, annot = False, cmap = color_map, vmax = 1, 
+            center = 0, square = True, linewidths = 0.1,
+            cbar_kws = {"shrink": 0.75})
+plt.title('Heat Map of Correlation Coefficient Matrix', fontsize = 18)
+plt.xlabel('Column Number from the Data Frame', fontsize = 12)
+plt.ylabel('Column Number from the Data Frame', fontsize = 12)
+plt.savefig('sub1_fig1.png')
+plt.show
+
+
+plt.figure(figsize=(8,8))
+color_map = sns.diverging_palette(230, 20, as_cmap = True)
+sns.heatmap(sub1_corr, annot = False, cmap = color_map, vmax = 1, 
+            center = 0, square = True, linewidths = 0.5,
+            cbar_kws = {"shrink": 0.75})
+plt.title('Heat Map of Correlation Coefficient Matrix', fontsize = 18)
+plt.xlabel('Column Number from the Data Frame', fontsize = 12)
+plt.ylabel('Column Number from the Data Frame', fontsize = 12)
+plt.savefig('sub1_fig2.png')
+plt.show
+
+"""
+Q9 - Normalize data using min-max method.
+"""
+# Calling min_max function
+sub1_normalized_corr_df = min_max(sub1_df_remain)
+
+"""
+Q10 - Split data into 80% training and 20% testing sets.
+"""
+X = sub1_normalized_corr_df
+y = sub1_state_labels
+
+# Splitting data into train and test models
+sub1_X_train, sub1_X_test, sub1_y_train, sub1_y_test = train_test_split(
+    X, 
+    y, 
+    random_state = None,
+    test_size = 0.2,
+    stratify = y)
+     
+"""
+Q11 & Q12 - Train and test data using Random Forest Classifier.
+"""
+# Random Forest config
+sub1_rf = RandomForestClassifier(
+    n_estimators = 10, 
+    max_depth = None, 
+    min_samples_split = 2, 
+    random_state = None)
+
+# Training data
+sub1_rf.fit(sub1_X_train, sub1_y_train)
+
+# Testing data
+sub1_pred = sub1_rf.predict(sub1_X_test)
+
+"""
+Q13 - Create confusion matrix and compute accuracy.
+"""
+# Confusion matrix creation
+conf_mat = confusion_matrix(sub1_y_test, sub1_pred)
+
+# Accuracy score
+sub1_accuracy = accuracy_score(sub1_y_test, sub1_pred)
+print('Accuracy Rate: {}%'.format(round(sub1_accuracy * 100, 1)))
+
+
+"""
+SUBJECT 2 TASKS
+"""
+"""
+Q1 - Load the data in each of the Matlab files into a data frame.
+"""
+# Setting current directory and getting files
+os.chdir('/Users/umreenimam/Documents/Masters/Masters_Classes/CS_5310/chapter05/lab_data/Data/Subject2')
+sub2_pre_file = 'Pre.mat'
+sub2_med_file = 'Med.mat'
+sub2_post_file = 'Post.mat'
+
+# Loading data 
+sub2_pre_data = loadmat(sub2_pre_file)
+sub2_med_data = loadmat(sub2_med_file)
+sub2_post_data = loadmat(sub2_post_file)
+
+# Extracting data
+sub2_pre_ = sub2_pre_data['data']
+sub2_med_ = sub2_med_data['data']
+sub2_post_ = sub2_post_data['data']
+
+# Create dataframes and transpose them 
+sub2_pre_t = pd.DataFrame(sub2_pre_.T)
+sub2_med_t = pd.DataFrame(sub2_med_.T)
+sub2_post_t = pd.DataFrame(sub2_post_.T)
+
+"""
+Q2 - Remove zero-fill-in rows from each data frame.
+"""
+# Calling remove_zeros function
+sub2_pre = remove_zeros(sub2_pre_t)
+sub2_med = remove_zeros(sub2_med_t)
+sub2_post = remove_zeros(sub2_post_t)
+
+"""
+Q3 - Compute alpha PSIs and spectral entropies.
+"""
+# Calling alpha psi function on each dataframe
+sub2_pre_df = get_psi_entropies(sub2_pre)
+sub2_med_df = get_psi_entropies(sub2_med)
+sub2_post_df = get_psi_entropies(sub2_post)
+
+"""
+Q4 & Q5 - Create list of brain state labels & combine dataframes into one.
+"""
+# Calling create_labels function
+sub2_state_labels = create_labels(sub2_pre_df, sub2_med_df, sub2_post_df)
+
+# Calling combine_datasets function
+sub2_df = combine_datasets(sub2_pre_df, sub2_med_df, sub2_post_df)
+
+"""
+Q6-Q8 - Create a correlation coefficient matrix of 68 channels, remove co-linearity, 
+and print new correlation matrix.
+"""
+# Running co-linearity function
+corrleation_mat, cols_to_remove = remove_colinearity(sub2_df, -0.9)
+sub2_df_remain = sub2_df.drop(columns = cols_to_remove, axis = 1)
+
+# Correlation matrix of remaining data 
+sub2_corr = sub2_df_remain.corr()
+
+# Heatmaps of data before removing co-linearity & after removing 
+# co-linearity
+sns.set_theme(style = "white")
+
+plt.figure(figsize=(8,8))
+color_map = sns.diverging_palette(230, 20, as_cmap = True)
+sns.heatmap(corrleation_mat, annot = False, cmap = color_map, vmax = 1, 
+            center = 0, square = True, linewidths = 0.1,
+            cbar_kws = {"shrink": 0.75})
+plt.title('Heat Map of Correlation Coefficient Matrix', fontsize = 18)
+plt.xlabel('Column Number from the Data Frame', fontsize = 12)
+plt.ylabel('Column Number from the Data Frame', fontsize = 12)
+plt.savefig('sub2_fig1.png')
+plt.show
+
+
+plt.figure(figsize=(8,8))
+color_map = sns.diverging_palette(230, 20, as_cmap = True)
+sns.heatmap(sub2_corr, annot = False, cmap = color_map, vmax = 1, 
+            center = 0, square = True, linewidths = 0.5,
+            cbar_kws = {"shrink": 0.75})
+plt.title('Heat Map of Correlation Coefficient Matrix', fontsize = 18)
+plt.xlabel('Column Number from the Data Frame', fontsize = 12)
+plt.ylabel('Column Number from the Data Frame', fontsize = 12)
+plt.savefig('sub2_fig2.png')
+plt.show
+
+"""
+Q9 - Normalize data using min-max method.
+"""
+# Calling min_max function
+sub2_normalized_corr_df = min_max(sub2_df_remain)
+
+"""
+Q10 - Split data into 80% training and 20% testing sets.
+"""
+X = sub2_normalized_corr_df
+y = sub2_state_labels
+
+# Splitting data into train and test models
+sub2_X_train, sub2_X_test, sub2_y_train, sub2_y_test = train_test_split(
+    X, 
+    y, 
+    random_state = None,
+    test_size = 0.2,
+    stratify = y)
+     
+"""
+Q11 & Q12 - Train and test data using Random Forest Classifier.
+"""
+# Random Forest config
+sub2_rf = RandomForestClassifier(
+    n_estimators = 10, 
+    max_depth = None, 
+    min_samples_split = 2, 
+    random_state = None)
+
+# Training data
+sub2_rf.fit(sub2_X_train, sub2_y_train)
+
+# Testing data
+sub2_pred = sub2_rf.predict(sub2_X_test)
+
+"""
+Q13 - Create confusion matrix and compute accuracy.
+"""
+# Confusion matrix creation
+conf_mat = confusion_matrix(sub2_y_test, sub2_pred)
+
+# Accuracy score
+sub2_accuracy = accuracy_score(sub2_y_test, sub2_pred)
+print('Accuracy Rate: {}%'.format(round(sub2_accuracy * 100, 1)))
